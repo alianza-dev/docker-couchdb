8800a186a306a480464d7dcfb2ca43cff1c42090
Luciano Rezende <lrezende@daitangroup.com>
2017-04-11 11:22:39 -0300

    Improve test script

--- 2.0.0/democluster.sh
+++ 2.0.0/democluster.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+rm -rf /tmp/couchdb
+
+mkdir -p /tmp/couchdb/node1
+mkdir -p /tmp/couchdb/node2
+mkdir -p /tmp/couchdb/node3
+
 docker rm -f couch1 couch2 couch3
 
 docker run -d \
@@ -9,6 +15,7 @@ docker run -d \
 	-e COUCHDB_USER=admin \
 	-e COUCHDB_PASSWORD=admin \
 	-e ERLANG_COOKIE=monster \
+	-v /tmp/couchdb/node1:/opt/couchdb/data \
 	--network=couchdb_net \
 	--network-alias=node1.couch.local \
 	--name=couch1 \
@@ -21,6 +28,7 @@ docker run -d \
 	-e COUCHDB_USER=admin \
 	-e COUCHDB_PASSWORD=admin \
 	-e ERLANG_COOKIE=monster \
+	-v /tmp/couchdb/node2:/opt/couchdb/data \
 	--network=couchdb_net \
 	--network-alias=node2.couch.local \
 	--name=couch2 \
@@ -33,17 +41,19 @@ docker run -d \
 	-e COUCHDB_USER=admin \
 	-e COUCHDB_PASSWORD=admin \
 	-e ERLANG_COOKIE=monster \
+	-v /tmp/couchdb/node3:/opt/couchdb/data \
 	--network=couchdb_net \
 	--network-alias=node3.couch.local \
 	--name=couch3 \
 	couchdb:2.0-alz
 
+sleep 1
 
-curl -X PUT http://localhost:15986/_nodes/couchdb@node2.couch.local -d {} -u admin
-curl -X PUT http://localhost:15986/_nodes/couchdb@node3.couch.local -d {} -u admin
+curl -X PUT http://localhost:15986/_nodes/couchdb@node2.couch.local -d {} -u admin:admin
+curl -X PUT http://localhost:15986/_nodes/couchdb@node3.couch.local -d {} -u admin:admin
 
-curl -X PUT http://localhost:15984/_global_changes -d {} -u admin
-curl -X PUT http://localhost:15984/_metadata -d {} -u admin
-curl -X PUT http://localhost:15984/_replicator -d {} -u admin
-curl -X PUT http://localhost:15984/_users -d {} -u admin
+curl -X PUT http://localhost:15984/_global_changes -d {} -u admin:admin
+curl -X PUT http://localhost:15984/_metadata -d {} -u admin:admin
+curl -X PUT http://localhost:15984/_replicator -d {} -u admin:admin
+curl -X PUT http://localhost:15984/_users -d {} -u admin:admin
 
